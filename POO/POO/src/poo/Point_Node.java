1c68dce18d880b015bcd66fec080392255f765e0
Unknown <joao.pedro.ss.russo@gmail.com>
2018-05-01 17:23:29 +0100

    Vejam só se os tupples e o point_node tem erros please

---
+++ POO/POO/src/poo/Point_Node.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Point_Node extends Point {
+
+	Point_Node next;
+	
+	Point_Node(int x, int y){
+		super(x, y);
+		this.next=null;
+	}
+	
+}
---
+++ POO/POO/src/poo/Tupple.java
@@ -0,0 +1,16 @@
+
+public class Tupple {
+	
+	int cost;
+	Point point_A;
+	Point point_B;
+	Tupple next;
+	
+	Tupple(int c, Point A, Point B){
+		this.cost=c;
+		this.point_A=C;
+		this.point_B=D;
+		this.next=null;
+	}
+
+}
